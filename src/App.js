6d4e0a2611c9579fef50b2e569ce1c682fe0cea7
Noresp <e.stromjordet@gmail.com>
2021-10-15 18:13:11 +0200

    bootstrap layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+        <Row >
+          <Col className="App-Navbar">
+            <div>
+              Navbar
+            </div>
+          </Col>
+        </Row>
+        <Row >
+          <Col xs={2} className="App-Sidebar">
+            <div>
+              Left sidebar
+            </div>
+          </Col>
+          <Col xs={10} className="App-content">
+            <div className="Main-content">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <Button onClick={() => sendAlert()}>Test</Button>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
+}
+
+function sendAlert() {
+  console.log("Hello world");
 }
 
 export default App;
