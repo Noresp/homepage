d934fef9c30586507898fdd39d0b947567edc47f
Noresp <e.stromjordet@gmail.com>
2022-06-03 16:44:14 +0200

    rewrite classes into functional components

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -2,31 +2,29 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import logo from 'assets/logo.svg';
 
-class Dashboard extends React.Component {
-    render() {
-        return <>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Now with support for Typescript!
-        </p>
-        <Button onClick={() => this.sendAlert()}>Test</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </>;
-    }
+function Dashboard() {
+  const sendAlert = () => {
+    console.log("Hello world");
+  }
 
-    sendAlert() {
-        console.log("Hello world");
-      }
+  return <>
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.tsx</code> and save to reload.
+    </p>
+    <p>
+      Now with support for Typescript!
+    </p>
+    <Button onClick={() => sendAlert()}>Test</Button>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </>;
 }
 
 export default Dashboard;
--- src/components/tier-lists.tsx
+++ src/components/tier-lists.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import tierListFireEmblem from 'assets/tier-list-fire-emblem.png';
 
-class TierLists extends React.Component {
-    render() {
-        return <>
-            <h2>
-                Tier lists
-            </h2>
-            <img src={tierListFireEmblem} alt="fire-emblem" />
-        </>;
-    }
+function TierLists() {
+    return <>
+        <h2>
+            Tier lists
+        </h2>
+        <img src={tierListFireEmblem} alt="fire-emblem" />
+    </>;
 }
 
 export default TierLists;
