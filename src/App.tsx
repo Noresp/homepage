0551f647bc9d357f19f5c0714a3f962a031be399
Noresp <e.stromjordet@gmail.com>
2022-06-03 08:30:14 +0200

    react router update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
-  Route
+  Route,
+  Routes
 } from "react-router-dom";
 
 //bootstrap
@@ -18,37 +18,31 @@ import About from 'components/about';
 function App() {
   return (
     <Router basename="/homepage">
-      <div className="App">
-        <Container fluid>
-          <Row >
-            <Col className="App-Navbar">
-              <div>
-                Navbar
-              </div>
-            </Col>
-          </Row>
-          <Row >
-            <Col xs={2} className="App-Sidebar">
-              <Sidebar />
-            </Col>
-            <Col xs={10} className="App-content">
-              <div className="Main-content">
-                <Switch>
-                  <Route path="/about">
-                    < About />
-                  </Route>
-                  <Route path="/tier-lists">
-                    <TierLists />
-                  </Route>
-                  <Route path="/">
-                    <Dashboard />
-                  </Route>
-                </Switch>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+    <div className="App">
+      <Container fluid>
+        <Row >
+          <Col className="App-Navbar">
+            <div>
+              Navbar
+            </div>
+          </Col>
+        </Row>
+        <Row >
+          <Col xs={2} className="App-Sidebar">
+            <Sidebar />
+          </Col>
+          <Col xs={10} className="App-content">
+            <div className="Main-content">
+                <Routes>
+                  <Route path="/about" element={<About />} />
+                  <Route path="/tier-lists" element={<TierLists />} />
+                  <Route path="/" element={<Dashboard />} />
+                </Routes>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
     </Router>
   );
 }
